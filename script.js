5254a8128b6e8bf362b0a82f96fdc297154d225f
Max <contact@slymax.com>
2022-05-27 01:29:15 +0200

    update manifest

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-window.addEventListener("load", () => {
-    chrome.tabs.query({ "currentWindow": true }, tabs => {
-        const urls = tabs.map(tab => tab.url);
-        const textarea = document.querySelector("textarea");
-        textarea.value = urls.join("\n");
-        textarea.select();
-    });
-    document.querySelector("button").addEventListener("click", () => {
-        const urls = document.querySelector("textarea").value.split("\n");
-        urls.map(url => chrome.tabs.create({ "url": url.trim(), "active": false }));
-    });
+chrome.tabs.query({ currentWindow: true }, tabs => {
+    const textarea = document.querySelector("textarea");
+    const urls = tabs.map(tab => tab.url);
+    textarea.value = urls.join("\n");
+    textarea.select();
+});
+document.querySelector("button").addEventListener("click", () => {
+    const urls = document.querySelector("textarea").value.split("\n");
+    urls.forEach(url => chrome.tabs.create({ url, active: false }));
 });

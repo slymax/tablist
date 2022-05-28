a118826da579a7fb3383416f029c6a8b625e0bcc
Max <contact@slymax.com>
2022-05-28 09:03:15 +0200

    search when urls are invalid

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const textarea = document.querySelector("textarea");
 chrome.tabs.query({ currentWindow: true }, tabs => {
-    const textarea = document.querySelector("textarea");
     const urls = tabs.map(tab => tab.url);
     textarea.value = urls.join("\n");
     textarea.select();
 });
-document.querySelector("button").addEventListener("click", () => {
-    const urls = document.querySelector("textarea").value.split("\n");
-    urls.forEach(url => chrome.tabs.create({ url, active: false }));
-});
+document.querySelector("button").onclick = () => {
+    textarea.value.split("\n").forEach(text => {
+        try {
+            const url = new URL(text).href;
+            chrome.tabs.create({ url, active: false });
+        } catch {
+            chrome.tabs.create({ active: false }, tab => {
+                chrome.search.query({ text, tabId: tab.id });
+            });
+        }
+    });
+};
